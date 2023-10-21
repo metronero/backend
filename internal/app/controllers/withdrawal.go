fabd612a0c9e5809475502e262e6f2c79239f814
Siren <siren@kernal.eu>
2023-10-21 19:54:11 +0300

    Remove unused packages

--- internal/app/controllers/withdrawal.go
+++ internal/app/controllers/withdrawal.go
@@ -3,16 +3,9 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
-	"time"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/jwtauth/v5"
-	"github.com/google/uuid"
 
 	"gitlab.com/metronero/backend/internal/app/queries"
-	"gitlab.com/metronero/backend/internal/utils/moneropay"
 	"gitlab.com/metronero/metronero-go/api"
-	"gitlab.com/metronero/metronero-go/models"
 )
 
 // Returns a list of withdrawals.
